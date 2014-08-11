52e6388fcc8a96a8c253b11ba176db24d8dc5ba1
darkpgx <darkpgx@gmail.com>
2014-08-11 06:26:02 +0000

    add diff rooms for each chat

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 var app = require('express')();
-var chat = [];
+var chat = {};
 app.set('view engine','ejs');
 app.get('/', function(req, res){
   res.render('index');
 });
+app.get('/r/:rmname', function(req, res) {
+  res.render('room',{name: req.params.rmname});
+}); 
 app.get('/chat', function(req, res){
-  chat.push({username: req.query.username, chat: req.query.chat});
+  console.log(req.query);
+  if(typeof req.query.rmname == 'string'){
+  if(typeof chat[req.query.rmname] !== 'object'){
+    chat[req.query.rmname] = [];
+    chat[req.query.rmname].push({username: req.query.username, chat: req.query.chat});
+  }
+  else{ chat[req.query.rmname].push({username: req.query.username, chat: req.query.chat});};
+  console.log(chat);};
   res.send('success');
 });
 app.get('/getchat', function(req, res){
-  res.send(chat);
+  res.send(chat[req.query.rmname]);
 });
 app.listen('3001');
